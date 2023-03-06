573a8433d352e994b364d557fd714368ce5ee2d3
Chukxz <109679877+Chukxz@users.noreply.github.com>
2023-03-06 20:49:10 +0100

    Commited recent changes in frame.py and added radio.py

--- frame.py
+++ frame.py
@@ -1,18 +1,15 @@
 from tkinter import *
-from PIL import ImageTk, Image
 
 root = Tk()
 root.title("Learn Coding")
 root.iconbitmap("./Images/mytkico.ico")
 
+frame = LabelFrame(root,padx=50,pady=50)
+frame.pack(padx=20,pady=20)
 
-
-
-
-
-
-
-
-
+b1 = Button(frame,text="Don't Click Here!")
+b1.grid(row=0,column=0)
+b2 = Button(frame,text="Or Here!")
+b2.grid(row=1,column=1)
 
 root.mainloop()
---
+++ radio.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+root = Tk()
+root.title("Learn Coding")
+root.iconbitmap("./Images/mytkico.ico")
+
+r=IntVar()
+r.set(2)
+
+def clicked(value):
+    global myLabel
+    myLabel.forget()
+    myLabel=Label(root,text=value)
+    myLabel.pack()
+
+Radiobutton(root,text="Option 1",variable=r,value=1,command=lambda:clicked(1)).pack()
+Radiobutton(root,text="Option 2",variable=r,value=2,command=lambda:clicked(2)).pack()
+
+myLabel=Label(root,text=r.get())
+myLabel.pack()
+
+
+root.mainloop()
