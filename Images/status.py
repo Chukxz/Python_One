a4299aa153218e6133451454e187a07fbff34586
Chukxz <109679877+Chukxz@users.noreply.github.com>
2023-03-06 19:37:58 +0100

    Corrected mistakes.

--- Images/status.py
+++ Images/status.py
@@ -5,16 +5,15 @@ root = Tk()
 root.title("Learn Coding")
 root.iconbitmap("./Images/mytkico.ico")
 
-my_img1 = ImageTk.PhotoImage(Image.open("./Images/Quantum_gravity.png"))
-my_img2 = ImageTk.PhotoImage(Image.open("./Images/Quantum_mechanics_logo_1.jpg"))
-my_img3 = ImageTk.PhotoImage(Image.open("./Images/Quantum_mechanics_logo_2.png"))
-my_img4 = ImageTk.PhotoImage(Image.open("./Images/myimgs.png"))
-my_img5 = ImageTk.PhotoImage(Image.open("./Images/the_green.jpg"))
+my_img1 = ImageTk.PhotoImage(Image.open("./Images/Quantum_mechanics_logo_1.jpg"))
+my_img2 = ImageTk.PhotoImage(Image.open("./Images/Quantum_mechanics_logo_2.png"))
+my_img3 = ImageTk.PhotoImage(Image.open("./Images/myimgs.png"))
+my_img4 = ImageTk.PhotoImage(Image.open("./Images/the_green.jpg"))
 
-image_list = [my_img1,my_img2,my_img3,my_img4,my_img5]
+image_list = [my_img1,my_img2,my_img3,my_img4]
 image_list_len = len(image_list)
 
-status = Label(root, text="Image 1 of " + str(image_list_len))
+status = Label(root, text="Image 1 of " + str(image_list_len), bd=1, relief=SUNKEN)
 
 my_label = Label(image=my_img1)
 my_label.grid(row=0,column=0,columnspan=3)
@@ -58,7 +57,7 @@ button_forward = Button(root, text=">>",command=lambda:forward(2))
 
 button_back.grid(row=1,column=0)
 button_exit.grid(row=1,column=1)
-button_forward.grid(row=1,column=2)
+button_forward.grid(row=1,column=2,pady=10)
 
 status.grid(row=2,column=0,columnspan=3)
 
