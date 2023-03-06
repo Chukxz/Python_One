0835a31c9721375d04f492a13e3849c652a24abc
Chukxz <109679877+Chukxz@users.noreply.github.com>
2023-03-07 00:10:30 +0100

    :)

--- Images/radio.py
+++ Images/radio.py
@@ -4,19 +4,35 @@ root = Tk()
 root.title("Learn Coding")
 root.iconbitmap("./Images/mytkico.ico")
 
-r=IntVar()
-r.set(2)
+# r=IntVar()
+# r.set(2)
+
+#Radiobutton(root,text="Option 1",variable=r,value=1,command=lambda:clicked(r.get())).pack()
+#Radiobutton(root,text="Option 2",variable=r,value=2,command=lambda:clicked(r.get())).pack()
+
+myFrame = LabelFrame(root,padx=10,pady=10)
+myFrame.pack(pady=20,padx=5)
+
+MODES_TOPPINGS = [
+    ("Pepperoni", "Pepperonin"),
+    ("Cheese", "Cheese"),
+    ("Mushroom", "Mushroom"),
+    ("Onion", "Onion")
+]
+
+pizza = StringVar()
+pizza.set("Pepperoni")
+
+for text, mode in MODES_TOPPINGS:
+    Radiobutton(myFrame,text=text,variable=pizza,value=mode).pack(anchor=W)
 
 def clicked(value):
-    global myLabel
-    myLabel.forget()
-    myLabel=Label(root,text=value)
+    myLabel = Label(myFrame,text=value)
     myLabel.pack()
 
-Radiobutton(root,text="Option 1",variable=r,value=1,command=lambda:clicked(r.get())).pack()
-Radiobutton(root,text="Option 2",variable=r,value=2,command=lambda:clicked(r.get())).pack()
-myLabel=Label(root,text=r.get())
-myLabel.pack()
+myButton = Button(myFrame,text="Click Me!",command=lambda:clicked(pizza.get())).pack(anchor=W)
+
+button_exit = Button(root, text="EXIT PROGRAM",command=root.quit).pack()
 
 
 root.mainloop()
