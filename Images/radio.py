a73a0d5bc593444c0560c4e232d105036973209f
Chukxz <109679877+Chukxz@users.noreply.github.com>
2023-03-06 21:59:19 +0100

    done.

--- Images/radio.py
+++ Images/radio.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 root = Tk()
 root.title("Learn Coding")
-root.iconbitmap("./Images/mytkico.ico")
+root.iconbitmap("../Images/mytkico.ico")
 
 r=IntVar()
 r.set(2)
@@ -13,9 +13,8 @@ def clicked(value):
     myLabel=Label(root,text=value)
     myLabel.pack()
 
-Radiobutton(root,text="Option 1",variable=r,value=1,command=lambda:clicked(1)).pack()
-Radiobutton(root,text="Option 2",variable=r,value=2,command=lambda:clicked(2)).pack()
-
+Radiobutton(root,text="Option 1",variable=r,value=1,command=lambda:clicked(r.get())).pack()
+Radiobutton(root,text="Option 2",variable=r,value=2,command=lambda:clicked(r.get())).pack()
 myLabel=Label(root,text=r.get())
 myLabel.pack()
 
