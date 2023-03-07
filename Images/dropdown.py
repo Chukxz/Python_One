fe0cdcaad305df7212b6929c2cbbe6892a69db59
Chukxz <109679877+Chukxz@users.noreply.github.com>
2023-03-07 06:18:56 +0100

    Finalized dropdown.py and created database.py

--- Images/dropdown.py
+++ Images/dropdown.py
@@ -4,5 +4,25 @@ root = Tk()
 root.iconbitmap("./Images/mytkico.ico")
 root.geometry("500x500")
 
+#Dropdown menu
+
+def show():
+    myLabel = Label(text=clicked.get()).pack()
+
+options = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday"
+]
+
+clicked = StringVar()
+clicked.set(options[0])
+
+drop1 = OptionMenu(root,clicked,*options)
+drop1.pack()
+
+myBtn = Button(root,text="Show selection",command=show).pack()
 
 root.mainloop()
